77b3e8aa3614ad08a972e61c7538c47dac57309c
Tomasz B <tomasz.bracik@gmail.com>
2023-01-10 14:51:01 +0100

    changes-to-frontend-03-auth

--- frontend/src/stores/auth.store.js
+++ frontend/src/stores/auth.store.js
@@ -4,7 +4,7 @@ import { fetchWrapper } from '@/helpers';
 import { router } from '@/router';
 import { useAlertStore } from '@/stores';
 
-const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
+// const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
 
 export const useAuthStore = defineStore({
     id: 'auth',
